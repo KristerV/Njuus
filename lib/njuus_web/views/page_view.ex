4c5b25cce58e305417f68358398d988c93197aba
Krister Viirsaar <git@krister.ee>
2019-04-28 22:10:04 +0300

    Remove noise from index

--- lib/njuus_web/views/page_view.ex
+++ lib/njuus_web/views/page_view.ex
@@ -1,3 +1,11 @@
 defmodule NjuusWeb.PageView do
   use NjuusWeb, :view
+
+  def get_link(post) do
+    if post.link == "" do
+      link "Show", to: Routes.post_path(@conn, :show, post)
+    else
+      post.link
+    end
+  end
 end
