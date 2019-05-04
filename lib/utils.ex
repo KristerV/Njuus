42c6f835d0c2f036f005d70092e1ae9bb94e74bb
Krister Viirsaar <git@krister.ee>
2019-05-04 15:19:47 +0300

    Add many more news feeds

---
+++ lib/utils.ex
@@ -0,0 +1,15 @@
+defmodule Utils do
+  def typeof(val) do
+    cond do
+      is_float(val) -> "float"
+      is_number(val) -> "number"
+      is_atom(val) -> "atom"
+      is_boolean(val) -> "boolean"
+      is_binary(val) -> "binary"
+      is_function(val) -> "function"
+      is_list(val) -> "list"
+      is_tuple(val) -> "tuple"
+      true -> "not sure"
+    end
+  end
+end
