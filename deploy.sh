3cc0df8a180edf2474483c03fcfb9e7ce4daf96d
Krister Viirsaar <git@krister.ee>
2019-05-05 18:27:32 +0300

    Deployment completed

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "--------------------- Compile with docker"
+docker run -v $(pwd):/opt/build --rm -it elixir-ubuntu:latest ./build.sh
+echo "--------------------- SCP to the server"
+scp -r rel/artifacts/njuus-0.1.0.tar.gz web@68.183.208.25:/home/web/
+echo "--------------------- Extract and restart service"
+ssh root@68.183.208.25 'cd /home/web/ && tar -xzvf njuus-0.1.0.tar.gz -C njuus && systemctl restart njuus'
+echo "--------------------- Done"
+ssh -t root@68.183.208.25 'journalctl'
