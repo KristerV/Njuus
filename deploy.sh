1c1488f10d8cf315eda3c0edbf4ae7c403b5a588
Krister Viirsaar <git@krister.ee>
2019-05-10 10:14:23 +0300

    Fancy logo title

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ docker run -v $(pwd):/opt/build --rm -it elixir-ubuntu:latest ./build.sh
 echo "--------------------- SCP to the server"
 scp -r rel/artifacts/njuus-0.1.0.tar.gz web@68.183.208.25:/home/web/
 echo "--------------------- Extract and restart service"
+# 1. clean out previous files
+# 2. scp and extract
+# 3. chown web:web
+# 4. start service
 ssh root@68.183.208.25 'cd /home/web/ && tar -xzvf njuus-0.1.0.tar.gz -C njuus && systemctl restart njuus'
 echo "--------------------- Done"
-ssh -t root@68.183.208.25 'journalctl -n 300'
+ssh -t root@68.183.208.25 'journalctl -n 300 -t njuus'
