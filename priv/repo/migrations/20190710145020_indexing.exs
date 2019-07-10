2679d3dda7dbf064a7b0f92897db247db4f48b1b
Krister Viirsaar <git@krister.ee>
2019-07-10 19:46:47 +0300

    Revert "Index a few more columns"
    This reverts commit 89ac2ab6ccddae70dcd196c6f7b8bca8ad51b646.

--- priv/repo/migrations/20190710145020_indexing.exs
+++
@@ -1,8 +0,0 @@
-defmodule Njuus.Repo.Migrations.Indexing do
-  use Ecto.Migration
-
-  def change do
-    create index(:posts, [:datetime, :source, :categories])
-    create index(:tracking, [:inserted_at])
-  end
-end
